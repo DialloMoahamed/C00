c16482497028643e04bbd82203a2c88c3bc5d2c2
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-05 23:10:55 +0100

    Correction Exo4

--- create_directory.sh
+++ create_directory.sh
@@ -1,20 +1,9 @@
 # Cette commande permet crée un répertoire nommé test_directory
 #!/bin/bash
 
-# Nom du répertoire à créer
-nom_repertoire="test_directory"
-
-# Vérifie si le répertoire existe déjà
-if [ -d "$nom_repertoire" ]; then
-  echo "Le répertoire '$nom_repertoire' existe déjà."
+if [ -d "test_directory" ]; then
+  echo "Le répertoire 'test_directory' existe déjà."
 else
-  # Crée le répertoire s'il n'existe pas
-  mkdir "$nom_repertoire"
-  if [ $? -eq 0 ]; then
-    echo "Le répertoire '$nom_repertoire' a été créé avec succès."
-  else
-    echo "Erreur lors de la création du répertoire '$nom_repertoire'."
-  fi
+  mkdir test_directory
+  echo "Le répertoire 'test_directory' a été créé."
 fi
-
-exit 0
