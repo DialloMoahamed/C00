b22cc27983aa821b2adaa3d7818e4af87c8aa8d5
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-06 13:23:34 +0100

    Correction Exo4

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Ce script vérifie si un fichier donné existe ou non.
+# Ce script vérifie si un fichier donné existe ou non
 
-read -p "Entrez le nom du fichier : " filename
+echo "Entrez le nom du fichier :"
+read filename
 
 if [ -f "$filename" ]; then
-    echo "Le fichier '$filename' existe."
+    echo "Le fichier '$filename' existe"
 else
-    echo "Le fichier '$filename' n'existe pas."
+    echo "Le fichier '$filename' n'existe pas"
 fi
