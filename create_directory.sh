7921acf41eb9ad7c24fd2b59372e059c7e1a977c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-06 16:04:01 +0100

    Correction

--- create_directory.sh
+++ create_directory.sh
@@ -2,9 +2,4 @@
 
 # Ce script crée un répertoire nommé test_directory
 
-if [ "test_directory" ]; then
-    echo "Le répertoire test_directory existe déjà."
-else
-    mkdir test_directory
-    echo "Le répertoire test_directory a été créé avec succès."
-fi
+mkdir test_directory
